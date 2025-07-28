098caa8b6ed7511b653205a3521db430d04301ee
MengZhaonan1998 <mengzn1998@gmail.com>
2025-07-28 18:16:27 -0400

    test integral method: Now QTT generation is integrated in QTT.py. And the integral idea is put in QTTM.py

--- Py/test_integral_TCI.py
+++ Py/test_integral_TCI.py
@@ -5,64 +5,60 @@ import matplotlib.pyplot as plt
 
 from rank_revealing import prrldu
 from interpolation import cur_prrldu
-from QTT import populate_tensor_fromfunction, union_rows_bounded, scatter_plot_f1f2, integral_qtt, value_query_QTT
-from tensor_cross import TT_CUR_L2R, cross_core_interp_assemble, TCI_2site, cross_inv_merge, TCI_union_two, single_core_interp_assemble
+from QTT import populate_tensor_fromfunction, QTT_Generation, union_rows_bounded, scatter_plot_f1f2, integral_qtt, value_query_QTT, Function_Collection
+from tensor_cross import TT_CUR_L2R, cross_core_interp_assemble, TCI_2site, cross_inv_merge, TCI_union_two, single_core_interp_assemble, Rank1_Nested_initIJ_gen
+from QTTM import QTTM_INTCONT
 
 ''' === Quantics representation construction === '''
 # Quantics construction
-quantic_repres = lambda x1,x2,x3,x4,x5,x6,x7,x8,x9,x10,x11,x12: x1/2 + x2/(2**2) + x3/(2**3) + x4/(2**4) + x5/(2**5) + x6/(2**6) + x7/(2**7) + x8/(2**8) + x9/(2**9) + x10/(2**10) + x11/(2**11) + x12/(2**12)
-func1 = lambda t: 1.2 * t ** 4 - 0.2 * np.sqrt(t) - 1 + 0.6 * np.sin(7.3 * np.pi * t)  #t ** 5 - 3 * t ** 3 + 10 * t -6 #5 * np.sin(-2 * np.pi * t) - 3 * np.exp(t)
-func2 = lambda t: -1.1 * t ** 7 - 12 + np.exp(3.1*t) - 0.81 * np.cos(6 * np.pi * t) - 2 * t ** 2 + 4 + np.tan(t)  #-10 * np.exp(-(t - 1) * (t - 1) / 2) - 2 * t ** 3 + 4 g_func = lambda t: func1(t) * func2(t)
-#func1 = lambda t: 1024 * t**8 * (1-t)**8 * (2*t - 1)**2
-#func2 = lambda t: 4096 * t**10 * (1-t)**10 * (1 - 6*t + 6*t**2)**2
-shape = (2,2,2,2,2,2,2,2,2,2,2,2)
-dim = len(shape)
-x_tensor = populate_tensor_fromfunction(shape, quantic_repres)
-f1_tensor = func1(x_tensor)
-f2_tensor = func2(x_tensor)
+func1 = Function_Collection[3]
+func2 = Function_Collection[4]
+dim = 12
+x_tensor, f1_tensor = QTT_Generation(func1, dim)
+_, f2_tensor = QTT_Generation(func2, dim)
 g_tensor = f1_tensor * f2_tensor
 scatter_plot_f1f2(x_tensor, g_tensor, f1_tensor, f2_tensor)
-pass
-
 
 ''' === Tensor cross interpolation for f1, f2, g === '''
 # Create initial (rank-1) interpolation I/J sets
-r_max = 1
-eps = 1e-14
-TTCores, TTCores_Cross, TTRank, Nested_I_rank1, Nested_J_rank1 = TT_CUR_L2R(f1_tensor, r_max, eps)
-Assemble_TTCore_Cross = cross_core_interp_assemble(f1_tensor, Nested_I_rank1, Nested_J_rank1, TTRank)
-for i in range(2 * len(TTRank) - 3):
-    diff_flag = (Assemble_TTCore_Cross[i] == TTCores_Cross[i]).all()
-    if (diff_flag == False):
-        print(f"Interpolation assembly error at {i}!")
-Nested_I_rank1[0] = []
-Nested_J_rank1[dim+1] = []
+Nested_I_rank1, Nested_J_rank1 = Rank1_Nested_initIJ_gen(f1_tensor)
 
 # TCI-2site of f1
-r_max = 5
+r_max = 4
 interp_I_f1, interp_J_f1, TTRank_f1, recon_f1 = TCI_2site(f1_tensor, 0, r_max, Nested_I_rank1, Nested_J_rank1)
 error = tl.norm(f1_tensor - recon_f1) / tl.norm(f1_tensor)
 print(f"Relative error of f1 QTT at r_max = {r_max}: {error}")
 
 # TCI-2site of f2
-r_max = 5
+r_max = 4
 interp_I_f2, interp_J_f2, TTRank_f2, recon_f2 = TCI_2site(f2_tensor, 0, r_max, Nested_I_rank1, Nested_J_rank1)
 error = tl.norm(f2_tensor - recon_f2) / tl.norm(f2_tensor)
 print(f"Relative error of f2 QTT at r_max = {r_max}: {error}")
 
 # TCI-2site of g
-r_max = 7
+r_max = 4
 interp_I_g, interp_J_g, TTRank_g, recon_g = TCI_2site(g_tensor, 0, r_max, Nested_I_rank1, Nested_J_rank1)
 error = tl.norm(g_tensor - recon_g) / tl.norm(g_tensor)
 print(f"Relative error of g QTT at r_max = {r_max}: {error}")
 
-# Assemble f1
-TT_cross_f1 = cross_core_interp_assemble(f1_tensor, interp_I_f1, interp_J_f1, TTRank_f1)
-TT_cores_f1 = cross_inv_merge(TT_cross_f1, dim, 1)
+# Get new g's I, J sets from f1 TCI and f2 TCI via the integral method
+interp_I_g_new, interp_J_g_new, TTRank_g_new, Zj_list = QTTM_INTCONT(f1_tensor, interp_I_f1, interp_J_f1, TTRank_f1,
+             f2_tensor, interp_I_f2, interp_J_f2, TTRank_f2,
+             3, 4, dim)
 
-# Assemble f2
-TT_cross_f2 = cross_core_interp_assemble(f2_tensor, interp_I_f2, interp_J_f2, TTRank_f2)
-TT_cores_f2 = cross_inv_merge(TT_cross_f2, dim, 1)
+TT_cross_g_TCI = cross_core_interp_assemble(g_tensor, interp_I_g, interp_J_g, TTRank_g)
+TT_cores_g_TCI = cross_inv_merge(TT_cross_g_TCI, dim, 1)
+error = tl.norm(g_tensor - tl.tt_to_tensor(TT_cores_g_TCI)) / tl.norm(g_tensor)
+print(f"Relative error of g QTT at r_max = {r_max}: {error}")
+
+TT_cross_g_new = cross_core_interp_assemble(g_tensor, interp_I_g_new, interp_J_g_new, TTRank_g_new)
+TT_cores_g_new = cross_inv_merge(TT_cross_g_new, dim, 1)
+error = tl.norm(g_tensor - tl.tt_to_tensor(TT_cores_g_new)) / tl.norm(g_tensor)
+print(f"Relative error of g new at r_max = {r_max}: {error}")
+pass
+
+
+
 
 ''' === Limitation of RED - Reconstruction, Evaluation, Decomposition === '''
 def red_test():
@@ -84,24 +80,31 @@ def red_test():
         print(f"Relative error of g RED at r_max = {r_max}: {error}")    
     return
 
-red_test()
-pass
+#red_test()
 
-''' === Test the idea of hierarchical integral === '''
 def hInt_firstTry():
+    # Assemble f1
+    TT_cross_f1 = cross_core_interp_assemble(f1_tensor, interp_I_f1, interp_J_f1, TTRank_f1)
+    TT_cores_f1 = cross_inv_merge(TT_cross_f1, dim, 1)
+
+    # Assemble f2
+    TT_cross_f2 = cross_core_interp_assemble(f2_tensor, interp_I_f2, interp_J_f2, TTRank_f2)
+    TT_cores_f2 = cross_inv_merge(TT_cross_f2, dim, 1)
+    
     # Let's first say how hInt can give optimal I pivots from left side compared with direct TCI
     interp_I_f1_copy = interp_I_f1.copy()
     interp_I_f2_copy = interp_I_f2.copy()
     interp_J_new = {}
     
     # Maximal TT-Rank for target TT after product
-    contract_core_number = 4
-    max_rank = 7
+    contract_core_number = 3
+    max_rank = 4
 
     # Hierarchical Integral Iteration
     passed_core_number = 0
     r_ = 1
     Zj_list = []
+    Csubset_list = []
     TTRank_new = [1]
     while passed_core_number < dim-1:
         # Do we need to integrate
@@ -135,9 +138,13 @@ def hInt_firstTry():
         TTint_matrix = tl.reshape(TTint_contract_f1f2, [mat_row, mat_col])    
         #_, diag, _, _, _, pr, pc, _ = prrldu(TTint_matrix, eps, 5)
         r_subset, c_subset, cross_inv, cross, rank, pr, pc = cur_prrldu(TTint_matrix, 0, max_rank)
+        Csubset_list.append(tl.reshape(c_subset, [r_, 2, rank]))
         Z = c_subset @ cross_inv 
-        Z_core = tl.reshape(c_subset, [r_, 2, rank])
+        Z_core = tl.reshape(Z, [r_, 2, rank])
         Zj_list.append(Z_core)
+        if passed_core_number == dim-2:
+            last_core = tl.reshape(r_subset, [rank, 2, 1])
+            Zj_list.append(last_core)
         
         r_ = rank
         TTRank_new.append(rank)
@@ -175,7 +182,7 @@ def hInt_firstTry():
     # !! To be discussed here... Use 1-site TCI?
     pass
     for i in range(dim-2, 0, -1):
-        ccore = Zj_list[i] 
+        ccore = Csubset_list[i] 
         cshape = ccore.shape
         zmat = tl.reshape(ccore, [cshape[0], cshape[1] * cshape[2]], order='F')
         _, _, _, _, _, rps, cps, _ = prrldu(zmat, 0, cshape[0]) 
@@ -189,44 +196,24 @@ def hInt_firstTry():
             J[j,0] = c_J_idx
         interp_J_new[i+1] = J
 
-    return interp_I_f1_copy, interp_J_new, TTRank_new
-
-# Get new g's I, J sets from f1 TCI and f2 TCI via the integral method
-interp_I_g_new, interp_J_g_new, TTRank_g_new = hInt_firstTry()
-
-TT_cross_g_TCI = cross_core_interp_assemble(g_tensor, interp_I_g, interp_J_g, TTRank_g)
-TT_cores_g_TCI = cross_inv_merge(TT_cross_g_TCI, dim, 1)
-error = tl.norm(g_tensor - tl.tt_to_tensor(TT_cores_g_TCI)) / tl.norm(g_tensor)
-print(f"Relative error of g QTT at r_max = {r_max}: {error}")
-
-TT_cross_g_new = cross_core_interp_assemble(g_tensor, interp_I_g_new, interp_J_g_new, TTRank_g_new)
-TT_cores_g_new = cross_inv_merge(TT_cross_g_new, dim, 1)
-error = tl.norm(g_tensor - tl.tt_to_tensor(TT_cores_g_new)) / tl.norm(g_tensor)
-print(f"Relative error of g new at r_max = {r_max}: {error}")
-pass
-
+    return interp_I_f1_copy, interp_J_new, TTRank_new, Zj_list
 
 # Plot numerical results
 max_rank_selection = [2,3,4,5,6,7,8]
 rel_error_TCI_g = [0.518606738251041, 0.03929485422630780, 0.005080629328349756, 0.0022383885452628596, 0.0001335697037330801, 6.460937515610282e-05, 5.4462581444022203e-05]
-rel_error_INT_g = [0.387611297071045, 0.03474862630575235, 0.007601452070678471, 0.0021256243069158304, 0.0002251037306136618, 2.463364284198022e-05, 0.013284752015896135]
+rel_error_INT_g = [0.387611297071045, 0.03474862630575235, 0.007601452070678471, 0.0021256243069158304, 0.0002251037306136618, 2.463364284198022e-05, 1.963423284112322e-05]
 max_rank_union = [4, 7]
 rel_error_UNI_g = [0.016386594417341145, 0.0001748485620257831]
 
 plt.figure()
-plt.scatter(max_rank_union, rel_error_UNI_g, label="Union-prrlu-f1f2", marker='o',color='orange')
-plt.scatter(max_rank_selection, rel_error_INT_g, label="Integral-Contraction-f1f2", marker='s',color='green')
-plt.scatter(max_rank_selection, rel_error_TCI_g, label="TCI-prrlu-2site (RED)", marker='x',color='red')
-plt.annotate("outlier here, due to\nunstable matrix inversion", 
-             xy=(8, rel_error_INT_g[6]),  # Point to annotate (rank=8, corresponding error)
-             xytext=(7.2, 0.05),          # Position of the text
-             arrowprops=dict(arrowstyle='->', color='red', lw=1.5),
-             fontsize=10,
-             color='red',
-             ha='center')
+#plt.scatter(max_rank_union, rel_error_UNI_g, label="Union-prrlu-f1f2", marker='o',color='orange')
+plt.plot(max_rank_selection, rel_error_INT_g,color='green')
+plt.scatter(max_rank_selection, rel_error_INT_g, label="QTTM", marker='s',color='green')
+#plt.scatter(max_rank_selection, rel_error_TCI_g, label="TCI-prrlu-2site (RED)", marker='x',color='red')
 plt.yscale("log")
 plt.grid()
 plt.legend()
-plt.xlabel("TT-Rank")
+plt.xlabel("Maximum TT-Rank")
 plt.ylabel("Relative difference from the real f1xf2")
 plt.savefig("relerr_UNI_vs_TCI_vs_INT.png")
+plt.savefig('relerr_UNI_vs_TCI_vs_INT.svg', format='svg', bbox_inches='tight')
