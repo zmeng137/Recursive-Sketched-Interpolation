463ed706e296228fa47159cf70b78de64ff2b92e
MengZhaonan1998 <mengzn1998@gmail.com>
2025-07-28 18:47:22 -0400

    remove function collection

--- Py/QTT.py
+++ Py/QTT.py
@@ -123,9 +123,3 @@ def value_query_QTT(QTT, TTRank, pos):
                 merge[i] += interm_core[j] * QTT[p+1][j, free, i] 
         interm_core = merge
     return interm_core[0]
-
-Function_Collection = {}
-Function_Collection[1] = lambda x: 1.2 * x ** 4 - 0.2 * np.sqrt(x) - 1 + 0.6 * np.sin(7.3 * np.pi * x)  
-Function_Collection[2] = lambda x: -1.1 * x ** 7 - 12 + np.exp(3.1 * x) - 0.81 * np.cos(6 * np.pi * x) - 2 * x ** 2 + 4 + np.tan(x) 
-Function_Collection[3] = lambda x: 3 * np.exp(- (x - 1) * (x - 1) / (2 * 0.3 * 0.3))
-Function_Collection[4] = lambda x: 3 * np.exp(- (x - 0) * (x - 0) / (2 * 0.3 * 0.3))
