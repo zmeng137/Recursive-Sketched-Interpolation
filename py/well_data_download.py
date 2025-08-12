a1461cd94823fdf9f1781672001749a81c08607f
MengZhaonan1998 <mengzn1998@gmail.com>
2025-08-12 13:42:52 -0400

    move files to data/

--- py/well_data_download.py
+++
@@ -1,7 +0,0 @@
-from the_well.utils.download import well_download
-
-base_path = "/mnt/CROSS/Well"  
-
-well_download(base_path=base_path, 
-              dataset="MHD_64", 
-              split="train")
