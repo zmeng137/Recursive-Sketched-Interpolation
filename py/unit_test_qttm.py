719211dc424931e7145c87c952c99c01a9ef9f28
MengZhaonan1998 <mengzn1998@gmail.com>
2025-09-14 22:02:15 -0400

    qttm unit test gaussian

--- py/unit_test_qttm.py
+++ py/unit_test_qttm.py
@@ -5,28 +5,31 @@ import time as tm
 import tensorly as tl
 import matplotlib.pyplot as plt
 
-from utils import load_quantics_tensor_hdf5, convert_quantics_tensor_to_1d, size_tt, load_quantics_tensor_formula
+from utils import load_quantics_tensor_hdf5, convert_quantics_tensor_to_1d, size_tt, load_quantics_tensor_formula, tt_to_tensor_tensordot
 from qttm import qttm_intcont_noeval, qttm_ric, tt_contraction_opcount
 
 sys.path.append(os.path.join(os.path.dirname(__file__), '..', 'MLA-Toolkit', 'py'))
 from tci import TT_CUR_L2R
 
 ''' Tensor Construction '''
-# Load quantics function tensors from hdf5 files 
-#qtensor_f1, metadata_f1 = load_quantics_tensor_hdf5("/home/zmeng5/QTTM/datasets/qtensor_well/qt_MHD64_0.hdf5")
-#qtensor_f2, metadata_f2 = load_quantics_tensor_hdf5("/home/zmeng5/QTTM/datasets/qtensor_well/qt_MHD64_1.hdf5")
-
 # Load quantics function tensors from synthetic formulas 
-qtensor_f1, _ = load_quantics_tensor_formula(0, 15)
-qtensor_f2, _ = load_quantics_tensor_formula(1, 15)
+#qtensor_f1, _ = load_quantics_tensor_formula(0, 15)
+#qtensor_f2, _ = load_quantics_tensor_formula(1, 15)
 
+# Load quantics function tensors from hdf5 files 
+filePath_f1 = "/home/zmeng5/QTTM/datasets/qtensor_gaussian/mix2d_gaussian_0.hdf5"
+filePath_f2 = "/home/zmeng5/QTTM/datasets/qtensor_gaussian/mix2d_gaussian_1.hdf5"
+qtensor_f1, metadata_f1 = load_quantics_tensor_hdf5(filePath_f1)
+qtensor_f2, metadata_f2 = load_quantics_tensor_hdf5(filePath_f2)
 
+# Real g = f1 * f2
+real_g = qtensor_f1 * qtensor_f2
 
 
 ''' Tensor Train Decomposition '''
 # TT-CUR Left->Right (One side)
-r_max_f1 = 100
-r_max_f2 = 100
+r_max_f1 = 15
+r_max_f2 = 15
 eps = 1e-14
 TTCore_f1, _, TTRank_f1, InterpSet_I_f1, _ = TT_CUR_L2R(qtensor_f1, r_max_f1, eps, 0, 0)
 TTCore_f2, _, TTRank_f2, InterpSet_I_f2, _ = TT_CUR_L2R(qtensor_f2, r_max_f2, eps, 0, 0)
@@ -49,23 +52,43 @@ error_f2 = tl.norm(qtensor_f2 - recon_f2) / tl.norm(qtensor_f2)
 print(f"Relative error: f1 (r_max = {r_max_f1}): {error_f1}; f2 (r_max = {r_max_f2}): {error_f2}")
 
 # QTTM: RED method
-start_t = tm.time()
-recon_g = recon_f1 * recon_f2  # g derived from RED
-TT_CUR_L2R(recon_g, 300, eps, 0, 0)
-end_t = tm.time()
-elapsed_ms = (end_t - start_t) * 1000
-print(f"Elapsed time: {elapsed_ms:.2f} ms")
+# (i) Reconstruction
+start_t_recon = tm.time()
+r_max = 30
+recon_f1 = tt_to_tensor_tensordot(TTCore_f1)
+recon_f2 = tt_to_tensor_tensordot(TTCore_f2)
+end_t_recon = tm.time()
 
-# Real g = f1 * f2
-real_g = qtensor_f1 * qtensor_f2
+# (ii) Evaluation (Multiplication)
+start_t_eval = tm.time()
+recon_g = recon_f1 * recon_f2  # g derived from RED
+end_t_eval = tm.time()
+
+# (iii) Decomposition 
+start_t_decomp = tm.time()
+TTCore_g_red, _, TTRank_g_red, InterpSet_I_g_red, _ = TT_CUR_L2R(recon_g, r_max, eps, 0, 0)
+end_t_decomp = tm.time()
+
+elapsed_ms_recon = (end_t_recon - start_t_recon) * 1000
+elapsed_ms_eval = (end_t_eval - start_t_eval) * 1000
+elapsed_ms_decomp = (end_t_decomp - start_t_decomp) * 1000
+print(f"Elapsed time of RED: Recon {elapsed_ms_recon:.2f} ms + Eval {elapsed_ms_eval:.2f} ms + Decomp {elapsed_ms_decomp:.2f} ms = {(elapsed_ms_recon + elapsed_ms_eval + elapsed_ms_decomp):.2f} ms")
+
+# Check accuracy
+recon_g_red = tl.tt_to_tensor(TTCore_g_red)
+error_g_red_recon = tl.norm(recon_g - recon_g_red) / tl.norm(recon_g)
+error_g_red_real =  tl.norm(real_g - recon_g_red) / tl.norm(real_g)
+print(f"TT-rank of new QTT of g: {TTRank_g_red}")
+print(f"Relative error (vs real g) at r_max = {r_max}: {error_g_red_real}")
+print(f"Relative error (vs recon g) at r_max = {r_max}: {error_g_red_recon}")
+
 
 # Get new g's I, J sets from f1 TCI and f2 TCI via the integral method
-contract_number = 9
-r_max = 200
-randomFlag = 0
+contract_number = 5
+r_max = 20
+randomFlag = 1
 seed = 0
-skLayer = 1
-start_t = tm.time()
+skLayer = 3
 
 #interp_I_g, interp_J_g, TTRank_g, TT_cores_g = qttm_intcont_noeval(
 #                TTCore_f1, InterpSet_I_f1, TTRank_f1,
@@ -77,9 +100,6 @@ interp_I_g, TTRank_g, TT_cores_g = qttm_ric(
                 TTCore_f2, InterpSet_I_f2, TTRank_f2,
                 contract_number, r_max, eps,
                 randomFlag, seed, skLayer)
-end_t = tm.time()
-elapsed_ms = (end_t - start_t) * 1000
-print(f"Elapsed time: {elapsed_ms:.2f} ms")
 
 error_vs_real = tl.norm(real_g - tl.tt_to_tensor(TT_cores_g)) / tl.norm(real_g)
 error_vs_recon = tl.norm(recon_g - tl.tt_to_tensor(TT_cores_g)) / tl.norm(recon_g)
@@ -131,4 +151,4 @@ axes[1,1].set_ylabel('X index')
 plt.colorbar(im4, ax=axes[1,1])
 
 plt.tight_layout()
-plt.savefig("active_matter_test.png")
+plt.savefig("gaussian_test.png")
