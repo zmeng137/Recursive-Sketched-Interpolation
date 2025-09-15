06e504b34b05db316059f050dc01d672554fe025
MengZhaonan1998 <mengzn1998@gmail.com>
2025-09-14 22:03:55 -0400

    gaussian2D function parameter

---
+++ py/data/gaussian2D_parameter.py
@@ -0,0 +1,56 @@
+# Define mixture components
+components = {}
+
+components[0] = [
+    {
+        'weight': 0.3,
+        'mu_x': -3,
+        'mu_y': 7,
+        'sigma_x': 3.5,
+        'sigma_y': 2.0,
+        'rho': 0.2
+    },
+    {
+        'weight': 0.3,
+        'mu_x': 6,
+        'mu_y': -8,
+        'sigma_x': 2.0,
+        'sigma_y': 2.5,
+        'rho': -0.4
+    },
+    {
+        'weight': 0.4,
+        'mu_x': -4,
+        'mu_y': 0,
+        'sigma_x': 3.0,
+        'sigma_y': 2.0,
+        'rho': 0.3
+    }
+]
+
+components[1] = [
+    {
+        'weight': 0.4,
+        'mu_x': 0,
+        'mu_y': 6,
+        'sigma_x': 3.0,
+        'sigma_y': 2.0,
+        'rho': 0.5
+    },
+    {
+        'weight': 0.5,
+        'mu_x': 3,
+        'mu_y': -4,
+        'sigma_x': 2.0,
+        'sigma_y': 2.0,
+        'rho': -0.3
+    },
+    {
+        'weight': 0.2,
+        'mu_x': 7,
+        'mu_y': 1,
+        'sigma_x': 1.5,
+        'sigma_y': 3.0,
+        'rho': 0.2
+    }
+]
