0a4dcc61f1559cda7e1261433252857a900ff579
MengZhaonan1998 <mengzn1998@gmail.com>
2025-11-01 17:00:04 -0400

    1d gaussian function with various pos

--- py/utils.py
+++ py/utils.py
@@ -8,14 +8,26 @@ from datetime import datetime
 
 # Synthetic function collection
 Function_Collection = {}
+
+# Normal functions
 Function_Collection[0] = lambda x: 1
 Function_Collection[1] = lambda x: 1.2 * x ** 4 - 0.2 * np.sqrt(x) - 1 + 0.6 * np.sin(7.3 * np.pi * x)  
 Function_Collection[2] = lambda x: -1.1 * x ** 7 - 12 + np.exp(3.1 * x) - 0.81 * np.cos(6 * np.pi * x) - 2 * x ** 2 + 4 + np.tan(x) 
 Function_Collection[3] = lambda x: 10 * np.exp(- (x - 1) * (x - 1) / (2 * 0.15 * 0.15))
 Function_Collection[4] = lambda x: 100 * np.exp(- (x - 0) * (x - 0) / (2 * 0.3 * 0.3))
+
+# High oscillation functions
 B_const = 7
 Function_Collection[5] = lambda x: np.cos(x * (2 ** B_const)) * np.cos(x * (2 ** B_const) / (4 * np.sqrt(5))) * np.exp(- x * x) + 2 * np.exp(-x)
 Function_Collection[6] = lambda x: np.sin(x * (2 ** B_const)) / (2 * np.sqrt(10)) * np.exp(- x * x * x) - 4 * np.exp(-np.sqrt(x))
+
+# 1D Gaussian functions
+w = 0.15
+w1,w2 = w,w
+x1 = 0.15
+x2 = 0.85
+Function_Collection[7] = lambda x: np.exp(-(x-x1)**2/w1**2)
+Function_Collection[8] = lambda x: np.exp(-(x-x2)**2/w2**2)
 
 # Save quantics tensor to HDF5 file with metadata.
 def save_quantics_tensor_hdf5(quantics_tensor, filepath, metadata=None, compression=True):
