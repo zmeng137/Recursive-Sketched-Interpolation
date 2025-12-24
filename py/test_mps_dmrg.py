77fab143607d5ce15dafd80ab757ef7532bcf300
MengZhaonan1998 <mengzn1998@gmail.com>
2025-12-24 00:28:39 -0500

    diagonal test

--- py/test_mps_dmrg.py
+++ py/test_mps_dmrg.py
@@ -7,23 +7,50 @@ from multiply_direct import HadamardTT_direct
 from tt_rounding import TT_rounding
 from error_eval import compute_tt_relative_error
 
-def mps_contract_ones(cores):
-    # Start with identity for the leftmost boundary
+def mps_all_contract(mps):
     result = np.array([[1.0]])
+
+    for core in mps:
+        result = result @ core
+
+    return result[0, 0]
+
+def mps_norm(mps):
+    n = len(mps)
+    mps_copy = mps.copy()
     
     # Contract each core with all-ones vector
-    for core in cores:
+    for i in range(n):
         # Sum over physical dimension: contract with all-ones
-        contracted = np.sum(core, axis=1)  # Shape: (r_i, r_{i+1})
-        # Contract with accumulated result
-        result = result @ contracted
+        mps_copy[i] = mps_copy[i][:,0,:] + mps_copy[i][:,1,:] + mps_copy[i][:,2,:]  #np.sum(mps_copy[i], axis=1)  # Shape: (r_i, r_{i+1})
+
+    norm = mps_all_contract(mps_copy)
+
+    return norm
+
+def mps_diagonal(mps_square):
+    n = len(mps_square)
+    mps_copy = mps_square.copy()
+    for i in range(n):
+        mps_copy[i] = np.sum(mps_copy[i], axis=1)  # Shape: (r_i, r_{i+1})
+
+    energy = 0.0
+    for j in range(n-1):
+        mps_copy_ = mps_copy.copy()
+        mps_copy_[j] = mps_square[j][:,0,:] - mps_square[j][:,2,:]  
+        mps_copy_[j+1] = mps_square[j+1][:,0,:] - mps_square[j+1][:,2,:]
     
-    return result[0, 0]
+        contrib = mps_all_contract(mps_copy_)
+        energy = energy + contrib
+
+    return energy
+
 
 def readh5_mps(filePath):   
     with h5py.File(filePath, "r") as f:
         num_sites = f["num_sites"][()]
         energy = f["energy"][()]
+        energy_diag = f["energy_diag"][()]
         
         mps = []
         for i in range(1, num_sites + 1):
@@ -47,39 +74,38 @@ def readh5_mps(filePath):
     mps_rank = [mps[0].shape[0]] + [mps[k].shape[2] for k in range(len(mps)-1)] + [mps[-1].shape[2]]
     print(f"\nMPS rank: {mps_rank}\n")
 
-    return mps, energy
+    return mps, energy, energy_diag
 
-filePath = "/home/zmeng5/QTTM/datasets/quantum_dmrg/psi_3.h5"
-mps, energy = readh5_mps(filePath)
-
-#mps_square_direct = HadamardTT_direct(mps, mps)
+filePath = "/home/zmeng5/QTTM/datasets/quantum_dmrg/psi_maxdim50_n50.h5"
+mps, energy, energy_diag = readh5_mps(filePath)
 
 print("\n == Evaluation of True func == \n")
-real_f = tl.tt_to_tensor(mps)
-real_g = real_f * real_f
 
-relerror_dict_rsi = {}
 Zdeviation_dict_rsi = {}
-r_max = [50]
+Zdeviation_dict_dir = {}
+Energydiag_dict_rsi = {}
+Energydiag_dict_dir = {}
+
+r_max = [100, 200, 300]
 contract_number = [2] 
 for co in contract_number:
     for rm in r_max:
         print("\n == RSI == \n")
         seed = 0
         eps=0
-        skdim = rm
+        skdim = int(rm/2)+20
         TTg_rsi, TTRank_rsi, _  = HadamardTT_RSI(mps, mps, co, rm, eps, skdim, seed)
         
         print("\n == Evaluation of Approx == \n")
-        g_rsi = tl.tt_to_tensor(TTg_rsi)
-        err_g_rsi = np.linalg.norm(real_g - g_rsi) / np.linalg.norm(real_g)
-        norm_rsi = mps_contract_ones(TTg_rsi)    
-        relerror_dict_rsi[f"cont_no={co}; rmax={rm}; skdim={skdim}"] = err_g_rsi
-        Zdeviation_dict_rsi[f"cont_no={co}; rmax={rm}; skdim={skdim}"] = 1 - norm_rsi
+        norm_rsi = mps_norm(TTg_rsi)    
+        energydiag_rsi = mps_diagonal(TTg_rsi)
+
+        Energydiag_dict_rsi[f"cont_no={co}; rmax={rm}; skdim={skdim}"] = np.abs(energy_diag - energydiag_rsi)
+        Zdeviation_dict_rsi[f"cont_no={co}; rmax={rm}; skdim={skdim}"] = np.abs(1 - norm_rsi)
 
 # Output errors
-print("\n === RSI Method Errors ===\n")
-for key, value in relerror_dict_rsi.items():
+print("\n === RSI Method Diag Energy deviation ===\n")
+for key, value in Energydiag_dict_rsi.items():
     print(f"{key}: {value}")
 print("\n =========================\n")
 
@@ -88,11 +114,37 @@ for key, value in Zdeviation_dict_rsi.items():
     print(f"{key}: {value}")
 print("\n =========================\n")
 
-'''
-inner(psi', H, psi)
-Hzz = \sum Sz_j Sz_j+1
-inner(psi', Hzz, psi)
-'''
+
+# Direct method
+# direct kronecker
+print("\n == direct == \n")
+TTg_direct = HadamardTT_direct(mps, mps)
+TTrank_direct = [TTg_direct[0].shape[0]] + [TTg_direct[k].shape[2] for k in range(len(TTg_direct)-1)] + [TTg_direct[-1].shape[2]]
+
+# Rounding recompression
+for rm in r_max:
+    print("\n == TT rounding == \n")
+    TTg_direct_trunc = TT_rounding(TTg_direct, 0, rm)   # Post re-compression
+    TTrank_direct_trunc = [TTg_direct_trunc[0].shape[0]] + [TTg_direct_trunc[k].shape[2] for k in range(len(TTg_direct_trunc)-1)] + [TTg_direct_trunc[-1].shape[2]]
+
+    print("\n == Evaluation of Approx == \n")
+    norm_dir = mps_norm(TTg_direct_trunc) 
+    energydiag_dir = mps_diagonal(TTg_direct_trunc)  
+    Zdeviation_dict_dir[f"direct rounding rmax={rm}"] = np.abs(1 - norm_dir)
+    Energydiag_dict_dir[f"direct rounding rmax={rm}"] = np.abs(energy_diag - energydiag_dir)
+
+print("\n === DIR Method Diag Energy deviation ===\n")
+for key, value in Energydiag_dict_dir.items():
+    print(f"{key}: {value}")
+print("\n =========================\n")
+
+
+print("\n === DIR Method Z deviation ===\n")
+for key, value in Zdeviation_dict_dir.items():
+    print(f"{key}: {value}")
+print("\n =========================\n")
+
+
 
 #mps_square_rounding = TT_rounding(mps_square_direct, 0, rmax)
 #approx_rsi = tl.tt_to_tensor(mps_square_rsi)
